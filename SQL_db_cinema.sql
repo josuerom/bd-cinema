57213003f7bb9589afb056a44d25fc1531a36aea
Josué Romero <79048209+josuerom@users.noreply.github.com>
2022-10-18 12:58:05 -0500

    Update SQL_db_cinema.sql

--- SQL_db_cinema.sql
+++ SQL_db_cinema.sql
@@ -1,11 +1,22 @@
+CREATE DATABASE cinema;
+USE cinema;
+
 CREATE TABLE Venta (
   IdVenta NOT NULL PRIMARY KEY IDENTITY(1,1),
-  Nombre VARCHAR(50) NOT NULL,
+  Producto VARCHAR(50) NOT NULL,
   Total MONEY
 );
 
 CREATE TABLE Concepto (
   Id NOT NULL PRIMARY KEY IDENTITY(1,1),
-  Producto VARCHAR(50) NOT NULL,
-  Cantidad VARCHAR(50) NOT NULL
+  Cantidad INT,
+  Precio Money
 );
+
+CREATE TRIGGER RecalcularTotal
+ON Concepto
+FOR UPDATE
+AS
+  DECLARE @registro INT = (SELECT Id FROM INSERTED)
+  UPDATE Venta SET Total = (SELECT SUM(Precio) FROM Concepto WHERE Id = @registro)
+  WHERE Id = @registro

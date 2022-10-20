9e8bde3a6ef58cb56a2efb901b244af947530572
Josué Romero <79048209+josuerom@users.noreply.github.com>
2022-10-20 12:54:10 -0500

    Delete SQLQueryTrigger.sql

--- SQLQueryTrigger.sql
+++
@@ -1,8 +0,0 @@
-CREATE TRIGGER RecalcularTotal
-ON Concepto
-FOR UPDATE
-AS
-  DECLARE @IdVenta int = (select Id from INSERTED)
-  UPDATE Venta SET Total = 
-    (select sum(precio) from Concepto WHERE Id = @IdVenta)
-  WHERE Id=@IdVenta
